6053054f6a2504cb1f0ed58c5325d9458e942266
jeff <alainnnsarmago@gmail.com>
2025-06-24 15:33:32 +0800

    fix: commented some debugging code

--- accounts/views.py
+++ accounts/views.py
@@ -27,23 +27,23 @@ def firebase_register_view(request):
       decoded = auth.verify_id_token(id_token)
       if not decoded.get("email_verified", False):
         return JsonResponse({"error": "Email not verified"}, status=403)
-      print("AUTH TOKEN: ", decoded)
+      # print("AUTH TOKEN: ", decoded)
       data = json.loads(request.body)
       email = decoded["email"]
       uid = decoded["uid"]
 
       # ✅ Now safe to query DB
       if UserProfile.objects.filter(email=email).exists() or UserProfile.objects.filter(firebase_uid=uid).exists():
-        print("USER ALREADY REGISTERED")
+        # print("USER ALREADY REGISTERED")
         return JsonResponse({"message": "Already registered"}, status=409)
       else: 
-        print("About to create user:")
-        print("  Email:", email)
-        print("  UID:", uid)
-        print("  First name:", data.get("first_name"))
-        print("  Last name:", data.get("last_name"))
-        print("  Role:", data.get("role"))
-
+        # print("About to create user:")
+        # print("  Email:", email)
+        # print("  UID:", uid)
+        # print("  First name:", data.get("first_name"))
+        # print("  Last name:", data.get("last_name"))
+        # print("  Role:", data.get("role"))
+        # print(" Avatar: ", data.get("avatar"))
         
         UserProfile.objects.create(
           firebase_uid=uid,
@@ -51,11 +51,11 @@ def firebase_register_view(request):
           first_name=data.get("first_name", ""),
           last_name=data.get("last_name", ""),
           role=data.get("role", ""),
-          avatar="",
+          avatar=data.get("avatar", ""),
         )
 
         user = UserProfile.objects.get(firebase_uid=uid)
-        print("Stored in DB:", user.first_name, user.last_name, user.role)
+        # print("Stored in DB:", user.first_name, user.last_name, user.role)
 
         return JsonResponse({"message": "User registered successfully"})
 
