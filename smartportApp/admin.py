59dfe1c6c6d13e70ad739f96a222d09ecfa56137
jeff <alainnnsarmago@gmail.com>
2025-07-02 13:51:46 +0800

    feat: add vessel and port model

--- smartportApp/admin.py
+++ smartportApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . models import Vessel, Port
 
 # Register your models here.
+admin.site.register(Vessel)
+admin.site.register(Port)
--- smartportApp/models.py
+++ smartportApp/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from accounts.models import UserProfile
 
 # Create your models here.
+class Port(models.Model):
+  port_id = models.AutoField(primary_key=True)
+  port_name = models.CharField(max_length=100)
+  port_code = models.CharField(max_length=10, unique=True)
+  latitude = models.FloatField()
+  longitude = models.FloatField()
+  port_description = models.TextField(blank=True)
+
+  def __str__(self):
+    return f"{self.port_name}" ({self.port_code})
+  
+class Vessel(models.Model):
+  vessel_id = models.AutoField(primary_key=True)
+  
+  class VesselStatus(models.TextChoices):
+    ARRIVED = "arrived", "Arrived"
+    IN_TRANSIT = "in_transit", "In Transit"
+    UNDER_MAINTENANCE = "maintenance", "Under Maintenance"
+    DELAYED = "delayed", "Delayed"
+
+  class VesselType(models.TextChoices):
+    CONTAINER = "container", "Container"
+    RO_RO = "ro_ro", "Ro-Ro"
+    GENERAL_CARGO = "general_cargo", "General Cargo"
+
+  name = models.CharField(max_length=100)
+  imo = models.CharField(max_length=20, unique=True)
+  vessel_type = models.CharField(
+    max_length=30,
+    choices=VesselType.choices,
+    default=VesselType.GENERAL_CARGO
+  )
+  status = models.CharField(
+    max_length=20,
+    choices=VesselStatus.choices,
+    default=VesselStatus.IN_TRANSIT
+  )
+  capacity = models.PositiveIntegerField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  created_by = models.ForeignKey(
+    UserProfile,
+    on_delete=models.SET_NULL,
+    null=True,
+    related_name="created_vessels"
+  )
+
+  def __str__(self):
+    return f"{self.name} (IMO {self.imo })"
