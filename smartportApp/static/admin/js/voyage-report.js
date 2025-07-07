fd5bad548ae2c01924cad1e81f7b9d6d8803fbd8
jeff <alainnnsarmago@gmail.com>
2025-07-07 16:23:51 +0800

    feat: save and print functionality

--- smartportApp/static/admin/js/voyage-report.js
+++ smartportApp/static/admin/js/voyage-report.js
@@ -35,6 +35,58 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Also expose it so you can rebind after pagination fetch:
   window.rebindVoyageCardEvents = attachClickEvents;
+
+  // HIDE BUTTONS
+  const hideButtons = (state) => {
+    if (state === "hide") {
+      document
+        .querySelectorAll(".print-hidden")
+        .forEach((el) => (el.style.display = "none"));
+    } else {
+      document
+        .querySelectorAll(".print-hidden")
+        .forEach((el) => (el.style.display = ""));
+    }
+  };
+
+  // ------------- PRINT BUTTON -------------
+  const printBtn = document.getElementById("printReport");
+  printBtn.addEventListener("click", () => {
+    window.print();
+  });
+
+  // ------------- PDF BUTTON -------------
+  const PDFBtn = document.getElementById("exportPdf");
+  PDFBtn.addEventListener("click", () => {
+    const reportElement = document.getElementById("voyageReportContent");
+    const voyageNumber =
+      document.querySelector('[data-field="voyageNumber"]')?.innerText ||
+      "voyage-report";
+    // hide the buttons:
+    hideButtons("hide");
+    const opt = {
+      margin: [1, 0, 0, 0],
+      filename: `${voyageNumber.trim().replace(/\s+/g, "-")}.pdf`,
+      image: { type: "jpeg", quality: 0.98 },
+      html2canvas: {
+        scale: 2,
+        scrollY: 0,
+        windowWidth: document.body.scrollWidth,
+        windowHeight: document.body.scrollHeight,
+      },
+      jsPDF: { unit: "in", format: "a4", orientation: "portrait" },
+      pagebreak: { mode: ["avoid-all", "css", "legacy"] },
+    };
+
+    html2pdf()
+      .set(opt)
+      .from(reportElement)
+      .save()
+      .then(() => {
+        // return the button
+        hideButtons("show");
+      });
+  });
 });
 
 const populateVoyageDetail = (data) => {
