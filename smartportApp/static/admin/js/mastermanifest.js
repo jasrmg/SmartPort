d57fcdf4aaf808f7dbcbac1942f2a5d8efb4cae2
jeff <alainnnsarmago@gmail.com>
2025-07-16 14:41:54 +0800

    feat: added master manifest and submanifest structure

---
+++ smartportApp/static/admin/js/mastermanifest.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("downloadPDF").addEventListener("click", () => {
+    html2pdf()
+      .set({
+        margin: 0.5,
+        filename: "master-manifest.pdf",
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: "in", format: "a4", orientation: "portrait" },
+      })
+      .from(document.getElementById("masterManifest"))
+      .save();
+  });
+});
