a1d9df4c29856ee54971cff7b15a0349effd2f13
jeff <alainnnsarmago@gmail.com>
2025-08-25 10:50:51 +0800

    feat: dynamic table data for the review submanifest

--- smartportApp/views_customs.py
+++ smartportApp/views_customs.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from . models import SubManifest
+
 def dashboard_view(request):
   context = {
     'show_logo_text': True,
@@ -6,8 +8,12 @@ def dashboard_view(request):
   return render(request, "smartportApp/custom/dashboard.html", context)
 
 def submanifest_review_view(request):
-  context = {
+  pending_submanifests = SubManifest.objects.filter(
+    status="pending_admin"
+  ).select_related("created_by", "voyage").order_by("-created_at")
 
+  context = {
+    'submanifests': pending_submanifests,
   }
   return render(request, "smartportApp/custom/submanifest-review.html", context)
 
